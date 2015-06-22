2e8d437dea6c32b27f71744129229925a7f58421
Mizureru <israel.lewis@mnscorp.net>
2015-06-22 12:35:12 +0100

    controller excercise

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
 angular.module('app', [])
 	.controller('demoController', function($scope){
-		$scope.name = 'Daniel'
-		$scope.sayHello = function(){
-			// alert('hello');
-			$scope.greeting = [
-				'hello',
-				$scope.name
-			].join(' ');
-		}
+		$scope.name = 'Daniel';
+		$scope.description = 'This is who I am';
+		$scope.profile = 'dog.jpg'
+		// $scope.sayHello = function(){
+		// 	// alert('hello');
+		// 	$scope.greeting = [
+		// 		'hello',
+		// 		$scope.name
+		// 	].join(' ');
+		// }
 	})
