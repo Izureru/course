5f34edeca3562f00d695bb973e2afa5b24538ae0
Mizureru <israel.lewis@mnscorp.net>
2015-06-23 11:27:26 +0100

    mario game functions within json

---
+++ mario.js
@@ -0,0 +1,50 @@
+var mario = {
+  description:"Small and jumpy. Likes princesses.",
+  height: 10,
+  weight: 3,
+  speed: 12,
+  attack_power: 5,
+  attack_name: "Hulk Smash"
+}
+mario.can_fly = true;
+
+var bowser = {
+  description: "Big and green, Hates princesses.",
+  height: 16,
+  weight: 6,
+  speed: 4,
+  boost:0,
+  attack_power: 2,
+  attack_name: "Bowser Bash"
+}
+var luigi = {
+  description:"Slighty taller but still jumpy.",
+  height: 14,
+  weight: 1,
+  speed: 14,
+  attack_power: 0,
+  attack_name: "Luigi Lunge"
+}
+
+var boss_battle = function(contestant_1, contestant_2) {
+	contestant_1 = mario;
+	contestant_2 = bowser;
+
+  if (contestant_1.attack_power >= contestant_2.attack_power) {
+    alert(contestant_1.attack_name + " Has won")
+  } else {
+    alert(contestant_2.attack_name + " Has won")
+  }
+}
+
+boss_battle();
+bowser.boost = 20
+boss_battle();
+mario.invincibilty = true;
+boss_battle();
+
+// alert(mario.description);
+// alert(bowser.description);
+
+
+// alert(luigi.description);
