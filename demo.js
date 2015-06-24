8e333fc9766bb05a884933145849d811ba61bb4b
Mizureru <israel.lewis@mnscorp.net>
2015-06-24 15:39:02 +0100

    directive and service working

--- demo.js
+++ demo.js
@@ -1,19 +1,39 @@
-angular.module('app',['flickr'])
-  .controller('demoController', function($scope, flickr) {
+angular.module('app',['flickr', 'cats'])
 
+
+
+angular.module('cats', [])
+  .directive('catDirective', function(){
+    var directive = {
+      scope:true,
+      controller: 'catController',
+      restrict:'AEC',
+      template: [
+        "<ul>",
+          '<li ng-repeat = "flicks in flickr.items">',
+            '<pre>',
+          '<img ng-src="{{flicks.media.m}}"/>',
+        '</pre>',
+          '</li>',
+        '</ul>"'
+      ].join('')
+    }
+    return directive;
+  })
+  .controller('catController', function($scope, flickr) {
     $scope.vm = this;
     $scope.vm.loading = true;
-  	flickr.getEvents().success(function(data){
-  		$scope.flickr = data;
+    flickr.getEvents().success(function(data){
+      $scope.flickr = data;
       $scope.vm.loading = false;
-  	});
-   })
+    });
+  });
 
-  angular.module('flickr', [])
-    .service('flickr', function($http){
-      var search = 'cats';
-  	 var service = {
-  		getEvents:function(){
+angular.module('flickr', [])
+  .service('flickr', function($http){
+    var search = 'cats';
+	  var service = {
+		getEvents:function(){
   			var url = [
             'http://api.flickr.com/services/feeds/photos_public.gne?tags=',
             search,
