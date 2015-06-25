8453f777422691145609bbb46279587fea1f6935
Mizureru <israel.lewis@mnscorp.net>
2015-06-25 10:58:31 +0100

    setup including transcluded text

--- demo.js
+++ demo.js
@@ -1,13 +1,22 @@
 angular.module('app', [])
-	.controller('demoController', function($scope){
-		$scope.name = 'Daniel';
-		$scope.description = 'This is who I am';
-		$scope.profile = 'dog.jpg'
-		// $scope.sayHello = function(){
-		// 	// alert('hello');
-		// 	$scope.greeting = [
-		// 		'hello',
-		// 		$scope.name
-		// 	].join(' ');
-		// }
-	})
+  .controller('demoController', function($scope) {
+
+  })
+  .directive('outerDirective', function() {
+    return {
+      scope: {},
+      // controller: function($scope) {
+      //   $scope.test = 'outer';
+      // },
+      // transclude:true,
+      // template: "I will Create a directive that will add a header and footer to any element. The title in the header will be bound to the isolate scope.",
+      // // template: '<span ng-transclude></span>',
+      // link: function(scope, element, attrs, controller, transclusion) {
+      //   transclusion(scope, function(clone, scope) {
+      //     element.append(clone);
+      //   });
+      // }
+      transclude: true,
+      template: "<div><header><h1>World of Template</h1></header><span ng-transclude></span></div>"
+    }
+  });
